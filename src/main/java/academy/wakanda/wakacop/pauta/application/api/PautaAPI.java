315ab367b86a8cd7283a22f79513a68574e17a1f
Manoel Mathias <dev.mathiasx@gmail.com>
2024-03-21 19:56:54 -0300

    implementa camada api e flow para service

---
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaAPI.java
@@ -0,0 +1,13 @@
+package academy.wakanda.wakacop.pauta.application.api;
+
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/pauta")
+public interface PautaAPI {
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    PautaCadastradaResponse cadastraPauta(@RequestBody @Valid NovaPautaRequest novaPautaRequest);
+}
---
+++ src/main/java/academy/wakanda/wakacop/pauta/application/api/PautaCadastradaResponse.java
@@ -0,0 +1,10 @@
+package academy.wakanda.wakacop.pauta.application.api;
+
+import lombok.Value;
+
+import java.util.UUID;
+
+@Value
+public class PautaCadastradaResponse {
+    private UUID id;
+}
